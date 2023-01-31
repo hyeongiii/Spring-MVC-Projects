4d4e4dcf3ad86ea673e90c427735c5b87197647d
hyeongiii <hyeongiii0811@gmail.com>
2023-01-31 16:29:32 +0900

    :white_check_mark: test: UserTest - password 실패케이스를 위한 테스트 성공

--- oop-practice/src/test/java/org/example/UserTest.java
+++ oop-practice/src/test/java/org/example/UserTest.java
@@ -23,4 +23,19 @@ class UserTest {
         // Why??  User 의 initPassword() 내부의 RandomPasswordGenerator 가 어떻게 동작하는지 해당 메서드 내부에서는 컨트롤 할 수 없기 때문
         assertThat(user.getPassword()).isNotNull();
     }
+
+    @DisplayName("패스워드가 요구사항에 부합하지 않아 초기화가 되지 않는다.")
+    @Test
+    void passwordTest2() {
+        // given
+        User user = new User();
+
+        // when
+        user.initPassword(new WrongFixedPasswordGenerator());
+
+        // then
+        // 테스트가 실패할 때도 있고, 성공할 때도 있다.
+        // Why??  User 의 initPassword() 내부의 RandomPasswordGenerator 가 어떻게 동작하는지 해당 메서드 내부에서는 컨트롤 할 수 없기 때문
+        assertThat(user.getPassword()).isNull();
+    }
 }
