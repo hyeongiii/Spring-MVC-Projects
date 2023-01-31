2a5321e977de62812b466c45e0d7251a2e7b17b6
hyeongiii <hyeongiii0811@gmail.com>
2023-01-31 16:21:17 +0900

    :white_check_mark: test: UserTest - passwordTest 성공

---
+++ oop-practice/src/test/java/org/example/UserTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @DisplayName("패스워드를 초기화한다.")
+    @Test
+    void passwordTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.initPassword(new CorrectFixedPasswordGenerator());
+
+        // then
+        // 테스트가 실패할 때도 있고, 성공할 때도 있다.
+        // Why??  User 의 initPassword() 내부의 RandomPasswordGenerator 가 어떻게 동작하는지 해당 메서드 내부에서는 컨트롤 할 수 없기 때문
+        assertThat(user.getPassword()).isNotNull();
+    }
+}
